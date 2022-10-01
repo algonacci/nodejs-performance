cfa00eae439101d03d149eee8494cc91bdef970d
algonacci <algonacci1@gmail.com>
2022-10-01 10:27:05 +0700

    testing logging and reload in pm2

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os");
 
 app = express();
 
@@ -18,18 +16,10 @@ app.get("/", (req, res) => {
 
 app.get("/timer", (req, res) => {
   // Delay the response
-  delay(9000);
-  res.send(`... ... ... Running on ${process.pid}`);
+  delay(4000);
+  res.send(`Beep ... Running on ${process.pid}`);
 });
 
 console.log("--- Running server.js ---");
-if (cluster.isMaster) {
-  console.log("Master has been started");
-  const NUM_WORKER = os.cpus().length;
-  for (let i = 0; i < NUM_WORKER; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log("Worker process started");
-  app.listen(3000);
-}
+console.log("Worker process started");
+app.listen(3000);
